623bcce01f23ed647603fd988a2963e50bc2a00c
Guanzhou <guanzhou.song@gmail.com>
2020-02-12 15:14:36 -0500

    iinit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ConfigProvider, DatePicker, message } from 'antd';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import 'antd/dist/antd.css';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+
+class App extends React.Component {
+    state = {
+        date: null,
+    };
+
+    handleChange = date => {
+        //alert(date);
+        message.info(`You pick: ${date ? date.format('YYYY-MM-DD') : 'unselected'}`);
+        this.setState({ date });
+    };
+    render() {
+        const { date } = this.state;
+        return (
+            <ConfigProvider>
+                <div style={{ width: 400, margin: '5px auto' }}>
+                    <label>This is a label, hello, world!</label>
+                    {/*<label><FontAwesomeIcon icon={faCoffee}></FontAwesomeIcon></label>*/}
+                </div>
+                <div style={{ width: 400, margin: '5px auto' }}>
+                    <label>This is a label with coffee icon<FontAwesomeIcon icon={faCoffee}></FontAwesomeIcon></label>
+                </div>
+                <div style={{ width: 400, margin: '5px auto' }}>
+                    <DatePicker onChange={this.handleChange} />
+                    <div style={{ marginTop: 20 }}>
+                        current date：{date ? date.format('YYYY-MM-DD') : 'Unselected'}
+                    </div>
+                </div>
+            </ConfigProvider>
+        );
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
